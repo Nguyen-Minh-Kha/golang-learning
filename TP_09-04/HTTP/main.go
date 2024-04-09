6edd41e6ef7394cc0a7f7a700e7f447b760b8de4
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2024-04-09 17:31:03 +0200

    TP HTTP

---
+++ TP_09-04/HTTP/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type User struct {
+	UserID   int
+	Login    string `json:"userName"`
+	Password string
+}
+
+var USERS map[string]User
+
+// Function to populate the global variable USERS from a JSON file 
+func populateUsers() map[string]User {
+	content, err := os.ReadFile("./users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var users []User
+	error := json.Unmarshal(content, &users)
+	if error != nil {
+		fmt.Println(err)
+	}
+
+	mapUsers := make(map[string]User)
+	for _, user := range users {
+		mapUsers["id"+strconv.Itoa((user.UserID))] = user
+	}
+
+	return mapUsers
+}
+
+// Function to serve the HTTP server
+func serve() {
+	defaultHandler := func(w http.ResponseWriter, r *http.Request) {
+		id := r.FormValue("id")
+		if user, ok := USERS[id]; ok {
+			response, err := json.Marshal(user)
+			if err != nil {
+				fmt.Println(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			w.Header().Set(
+				"Content-Type",
+				"application/json; charset=utf-8",
+			)
+			w.WriteHeader(http.StatusOK)
+			w.Write(response)
+		} else {
+			w.WriteHeader((http.StatusNotFound))
+		}
+	}
+	http.HandleFunc("/", defaultHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	USERS = populateUsers()
+
+	serve()
+}
