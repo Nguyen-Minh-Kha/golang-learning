f312754e7f035f1ff8c1643dc7c9523bdf12ee7b
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2024-04-09 15:59:49 +0200

    TP json

---
+++ TP_09-04/json/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type User struct {
+	Login string `json:"userName"`
+	Password string
+}
+
+func ex1 () {	
+	user := User{Login: "Paul", Password: "pass123"}
+	b, err := json.Marshal(user)
+	if err!= nil {
+        fmt.Println(err)
+    }
+	fmt.Println(string(b))
+}
+
+func ex2 () {
+	content, err := os.ReadFile("./users.json")
+	if err!= nil {
+        fmt.Println(err)
+    }
+
+	var users []User
+	
+	error := json.Unmarshal(content, &users)
+	
+	if error!= nil {
+        fmt.Println(err)
+    }
+	fmt.Printf("%+v", users)
+}
+
+func main() {
+    ex1()
+
+	ex2()
+	
+}
