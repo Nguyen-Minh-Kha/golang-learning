d0913505260b9b61a7c388b1fec137ad3ae4322e
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2024-04-09 14:50:11 +0200

    empty interface

---
+++ TP_09-04/interface_vide/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+)
+
+type emptyInterface interface {}
+
+func PrintIt (input interface{}) {
+	switch v := input.(type) {
+		case string:
+            fmt.Println("The type is string")
+        case int:
+            fmt.Println("The type is int")
+		default:
+            fmt.Printf("idk this type %T\n", v)
+	}
+}
+func main() {
+	x := 123
+    PrintIt(x)
+}
